de466cf94573cdc69bc479f20cec53513a8bbf10
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-23 16:06:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,6 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  # def initialize
-  #   @name = name
-  #   @artist_name = artist_name
-  # end
-
   def self.all
     @@all
   end
